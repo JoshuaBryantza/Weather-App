74c654ca685bc625cfaf4ba8af8b9284b714584c
Joshua Bryant <joshuabryantza@gmail.com>
2024-04-16 15:33:56 -0700

    Added the front- end and js with the css

---
+++ Assets/js/script.js
@@ -0,0 +1,169 @@
+const searchButton = document.querySelector(".search-btn");
+const cityInput = document.querySelector(".city-input");
+const currentWeatherData = document.querySelector(".current-weather");
+let currentDate = dayjs().format('MMM-DD-YYYY');
+const API_KEY = "313e19582894eb8e201b929fa986e291";
+const weatherCardsDiv = document.querySelector(".weather-cards");
+let contHistEl = $('.cityHist');
+const current_Location = document.querySelector(".location-btn");
+const cityList = [];
+const butt_arr = [];
+
+var timeDisplayEl = $('#time-display');
+
+function displayTimeDashBoard() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+  setInterval(displayTimeDashBoard, 1000);
+}
+displayTimeDashBoard();
+
+
+function getWeather(citiName, latitude, longtitude) {
+  const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${API_KEY}`;
+  fetch(WEATHER_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+
+      $('.weather-detail h3').text(citiName.toUpperCase() + " (" + currentDate + ")");
+      $('.2').text((((data.main.temp - 273.15) * 9 / 5) + 32).toFixed(0) + "°F");
+      $('.3').text(data.wind.speed);
+      $('.4').text(data.main.humidity);
+      $('weather-icon').attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`);
+      $('.weather-icon h4').text(data.weather[0].description.toUpperCase());
+
+    })
+}
+
+
+function getForecast(lat1, lon1) {
+
+  const FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat1}&lon=${lon1}&appid=${API_KEY}`;
+
+  fetch(FORECAST_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+
+      let foreCastDataList = [];
+      console.log(data);
+      const fiveDay_forecast = [];
+      for (let i = 0; i < data.list.length; i++) {
+        const foreCastDate = new Date(data.list[i].dt_txt).getDate();
+
+        if (!fiveDay_forecast.includes(foreCastDate)) {
+          fiveDay_forecast.push(foreCastDate);
+          foreCastDataList.push(data.list[i]);
+        }
+
+      }
+
+      for (let index = 0; index < fiveDay_forecast.length; index++) {
+        $('.card' + index + ' h3').text(foreCastDataList[index].dt_txt.split(" ")[0]);
+        var url_icon = "https://openweathermap.org/img/wn/" + foreCastDataList[index].weather[0].icon + "@2x.png";
+        $('.card' + index + ' img').attr('src', url_icon);
+        $(('.card' + index) + ' .2').text((((foreCastDataList[index].main.temp - 273.15) * 9 / 5) + 32).toFixed(0) + "°F");
+        $(('.card' + index) + ' .3').text(foreCastDataList[index].wind.speed);
+        $(('.card' + index) + ' .4').text(foreCastDataList[index].main.humidity);
+
+      }
+    })
+}
+
+
+function getCitylocation() {
+  $(".days-forecast").removeClass("hidden");
+  currentWeatherData.classList.remove("hidden");
+  var cityName = cityInput.value.trim();
+  if (!cityName) return;
+  const GEO_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+  fetch(GEO_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (!data.length)
+        return alert(`${cityName} is invalid, please re-check the input`);
+
+      const { name, lat, lon } = data[0];
+
+      const data_object = {
+        cityName: name,
+        latitude: lat,
+        longtitude: lon
+      };
+
+      if (!butt_arr.includes(cityName)) {
+        butt_arr.push(cityName);
+        cityList.push(data_object);
+      }
+
+      localStorage.setItem('city', JSON.stringify(cityList));
+      console.log(cityList.length + " length");
+      getWeather(cityName, lat, lon);
+      getForecast(lat, lon);
+      getHistory();
+    })
+    .catch(() => {
+      alert("An error occured while fetching the coordinates!");
+    });
+}
+
+
+const getUserCoordinates = () => {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude, longitude } = position.coords;
+      const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+      fetch(API_URL).then(response => response.json()).then(data => {
+        const { name } = data[0];
+        getWeather(name, latitude, longitude);
+        getForecast(latitude, longitude);
+      }).catch(() => {
+        alert("An error occurred while fetching the city name!");
+      });
+    },
+    error => {
+      if (error.code === error.PERMISSION_DENIED) {
+        alert("Geolocation request denied. Please reset location permission to grant access again.");
+      } else {
+        alert("Geolocation request error. Please reset location permission.");
+      }
+    });
+}
+
+function getHistory() {
+
+  contHistEl.empty();
+  for (let i = 0; i < cityList.length; i++) {
+
+    var btnEl = $('<button>').text(`${cityList[i].cityName}`)
+
+    btnEl.addClass('btn btn-outline-secondary histBtn');
+    btnEl.attr('id', i);
+    btnEl.attr('type', 'button');
+    contHistEl.append(btnEl);
+
+  }
+
+  var retrievedObject = localStorage.getItem('city');
+  var parsedObject = JSON.parse(retrievedObject);
+
+
+  $('.histBtn').on("click", function (event) {
+    event.preventDefault();
+
+    getWeather(parsedObject[$(this).attr('id')].cityName, parsedObject[$(this).attr('id')].latitude, parsedObject[$(this).attr('id')].longtitude);
+    getForecast(parsedObject[$(this).attr('id')].latitude, parsedObject[$(this).attr('id')].longtitude);
+
+
+  });
+}
+
+
+searchButton.addEventListener('click', getCitylocation);
+current_Location.addEventListener('click', getUserCoordinates);
---
+++ README.md
@@ -0,0 +1,31 @@
+# Weather-Dashboard 
+
+## Description
+
+- A travellers best tool to knowing what to expect, weather wise
+- This application allows users to know the current weather at their current location or where they are headed.
+
+
+## Features 
+
+- Search for a location to get the current weather conditions and the next 5 day forcast. 
+- The application will display temperature, wind speed, humidity all with helpful weather icons.
+- All of your looked up searches will be saved to a list, so going back and forth will be easy.
+
+
+## Installation 
+
+- All you have to do is clone this repository and open the index.html file in your browser.
+
+
+## How to use
+
+- Search a location: enter a location in the search bar.
+- Click "Use Current Location" to get your current locations weather.
+- Both of the above will display a 5 day forecast.
+- All of your searches will be listed below.
+
+
+## Credits 
+
+- OpenWeather API 
